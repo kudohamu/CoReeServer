446100feb41ab76c564fd285ff4049c436383f78
kudohamu <kudohamu91086@yahoo.co.jp>
2015-06-13 16:09:41 +0900

    動作確認用のアクションを作成

--- app/controllers/api/clothes_controller.rb
+++ app/controllers/api/clothes_controller.rb
@@ -1,2 +1,13 @@
 class Api::ClothesController < ApplicationController
+  before_filter :login_check
+
+  def hoge
+    cloth = Cloth.create(
+      name: "ほげ"
+    )
+
+    @result = if(cloth) then "success" else "failed" end
+
+    render formats: [:json], handlers: [:jbuilder]
+  end
 end
